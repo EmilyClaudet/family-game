c550d0642a386c2fcb37cf8afd9c58194e972161
Emily <emily@kano.me>
2016-08-13 18:06:17 -0400

    Revert "changed title to Claudet Family"

--- src/conf.lua
+++ src/conf.lua
@@ -8,10 +8,10 @@ function love.conf(t)
 	t.modules.timer = true
 	t.modules.mouse = true
 	t.modules.sound = true
-	t.title = "Claudet Family"
+	t.title = "Claudet Family Game"
 	t.author = "Claudet Family"
 	t.window.vsync = false
 	t.window.fsaa = 0
-	t.window.height = 600
-	t.window.width = 800
+	t.window.height = 768
+	t.window.width = 1268
 end
