eaba7fedb5184cf6eadbadd45c670a76bf8939af
EmilyClaudet <emily@kano.me>
2016-08-13 17:58:50 -0400

    changed title to Claudet Family

--- src/conf.lua
+++ src/conf.lua
@@ -8,7 +8,7 @@ function love.conf(t)
 	t.modules.timer = true
 	t.modules.mouse = true
 	t.modules.sound = true
-	t.title = "Claudet Family Game"
+	t.title = "Claudet Family"
 	t.author = "Claudet Family"
 	t.window.vsync = false
 	t.window.fsaa = 0
