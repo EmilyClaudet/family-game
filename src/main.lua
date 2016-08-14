8cbccb48cfb0e54a83ffb5086a78721ad36b439f
Catherine <cmclaudet@gmail.com>
2016-08-14 11:29:03 -0400

    Added idle animation and changed sprite structure

--- src/main.lua
+++ src/main.lua
@@ -1,96 +1,79 @@
 local STI = require "libs.STI"
-local avatar
 local playerX = 200
 local playerY = 200
-local speed = 200
+local playerspeed = 200
 local world = love.physics.newWorld(0, 0)
-local frames = {}
-local activeFrame
-local currentFrame = 1
-local elapsedTime = 0
-
--- constants
-local DOWN = 1
-local RIGHT = 2
-local LEFT = 3
-local UP = 4
+local booboo = require("booboo") --adds booboo sprite data
+require "handling" --adds functions getinstance, updateinstance, drawinstance
 
 function love.load()
-	-- load map file 
+	-- load map file
 	map = STI.new("res/maps/house.lua", {"box2d"})
 	music = love.audio.newSource("res/audio/mrbluesky.mp3")
-	avatar = love.graphics.newImage("res/images/avatar.png")
 
-    frames[DOWN] = {}
-    frames[DOWN][1] = love.graphics.newQuad(0, 0, 32, 32, avatar:getDimensions())
-    frames[DOWN][2] = love.graphics.newQuad(32, 0, 32, 32, avatar:getDimensions())
-    frames[DOWN][3] = love.graphics.newQuad(64, 0, 32, 32, avatar:getDimensions())
-    frames[DOWN][4] = love.graphics.newQuad(96, 0, 32, 32, avatar:getDimensions())
+	booboo = getinstance(booboo.attributes())		--instatiates booboo sprite
 
-    frames[RIGHT] = {}
-    frames[RIGHT][1] = love.graphics.newQuad(0, 32, 32, 32, avatar:getDimensions())
-    frames[RIGHT][2] = love.graphics.newQuad(32, 32, 32, 32, avatar:getDimensions())
-    frames[RIGHT][3] = love.graphics.newQuad(64, 32, 32, 32, avatar:getDimensions())
-    frames[RIGHT][4] = love.graphics.newQuad(96, 32, 32, 32, avatar:getDimensions())
-
-    frames[LEFT] = {}
-    frames[LEFT][1] = love.graphics.newQuad(0, 64, 32, 32, avatar:getDimensions())
-    frames[LEFT][2] = love.graphics.newQuad(32, 64, 32, 32, avatar:getDimensions())
-    frames[LEFT][3] = love.graphics.newQuad(64, 64, 32, 32, avatar:getDimensions())
-    frames[LEFT][4] = love.graphics.newQuad(96, 64, 32, 32, avatar:getDimensions())
-
-    frames[UP] = {}
-    frames[UP][1] = love.graphics.newQuad(0, 96, 32, 32, avatar:getDimensions())
-    frames[UP][2] = love.graphics.newQuad(32, 96, 32, 32, avatar:getDimensions())
-    frames[UP][3] = love.graphics.newQuad(64, 96, 32, 32, avatar:getDimensions())
-    frames[UP][4] = love.graphics.newQuad(96, 96, 32, 32, avatar:getDimensions())
-
-    direction = DOWN
-    activeFrame = frames[DOWN][currentFrame]
-
+	--player table allows easy access for player attributes and controls
+	player = {
+		x = playerX,
+		y = playerY,
+		speed = playerspeed,
+		char = booboo,
+		down = "down",
+		right = 'right',
+	  left = 'left',
+		up = "up"
+	}
 end
 
 function love.update(dt)
 	-- Update world
 	map:update(dt)
+	--Update player character
+	updateinstance(player.char,dt)
 
-    elapsedTime = elapsedTime + dt
+	--update character position based on where player moves
+	--changes player animation through changing char element
+	if (love.keyboard.isDown(player.down)) then
+			player.char.curr_anim = player.char.curr_sprite["animations"]["down"]
+      player.y = player.y + (player.speed * dt)
+  end
+	if (love.keyboard.isDown(player.right)) then
+			player.char.curr_anim = player.char.curr_sprite["animations"]["right"]
+      player.x = player.x + (player.speed * dt)
+  end
+	if (love.keyboard.isDown(player.left)) then
+			player.char.curr_anim = player.char.curr_sprite["animations"]["left"]
+      player.x = player.x - (player.speed * dt)
+  end
+  if (love.keyboard.isDown(player.up)) then
+			player.char.curr_anim = player.char.curr_sprite["animations"]["up"]
+      player.y = player.y - (player.speed * dt)
+  end
 
-    if (elapsedTime > 0.1) then
-        if(currentFrame < 4 ) then
-            currentFrame = currentFrame + 1
-        else
-            currentFrame = 1
-        end
-        elapsedTime = 0
-    end
+  if(love.keyboard.isDown('escape')) then
+      love.event.quit()
+  end
+end
 
-	if (love.keyboard.isDown('left')) then
-        playerX = playerX - (speed * dt)
-        direction = LEFT
-    end
-
-    if (love.keyboard.isDown('right')) then
-        playerX = playerX + (speed * dt)
-        direction = RIGHT
-    end
-
-    if (love.keyboard.isDown('up')) then
-        playerY = playerY - (speed * dt)
-        direction = UP
-    end
-
-    if (love.keyboard.isDown('down')) then
-        playerY = playerY + (speed * dt)
-        direction = DOWN
-    end
-
-    if(love.keyboard.isDown('escape')) then
-        love.event.quit()
-    end
-
-    activeFrame = frames[direction][currentFrame]
-
+function love.keyreleased(key)
+--puts character in idle state if player releases walking keys.
+	if key == player.down then
+		player.char.curr_frame = 1
+		player.char.curr_anim = player.char.curr_sprite["animations"]["downidle"]
+	end
+	if key == player.right then
+		player.char.curr_frame = 1
+		player.char.curr_anim = player.char.curr_sprite["animations"]["rightidle"]
+	end
+	if key == player.left then
+		player.char.curr_frame = 1
+		player.char.curr_anim = player.char.curr_sprite["animations"]["leftidle"]
+	end
+	if key == player.up then
+		player.char.curr_frame = 1
+		player.char.curr_anim = player.char.curr_sprite["animations"]["upidle"]
+	end
 end
 
 function love.draw()
@@ -98,9 +81,9 @@ function love.draw()
     map:draw()
     -- map:box2d_draw(map)
     map:setDrawRange(5, 5, 256, 256)
-	-- map:box2d_init(world)
+		-- map:box2d_init(world)
     -- Draw player
-    love.graphics.draw(avatar, activeFrame, playerX, playerY, 0)
+    drawinstance(player.char,player.x,player.y)
     -- Play music
     music:play()
 end
@@ -108,5 +91,3 @@ end
 function love.quit()
     print('Quitting Family Game...')
 end
-
-
